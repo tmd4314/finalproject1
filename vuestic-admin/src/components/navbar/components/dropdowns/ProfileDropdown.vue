57b950a59d631f80205cb0e766f6a7ba5318525c
tmd4314 <tmd4314@naver.com>
2025-06-04 15:25:04 +0900

    헤더 수정

--- vuestic-admin/src/components/navbar/components/dropdowns/ProfileDropdown.vue
+++ vuestic-admin/src/components/navbar/components/dropdowns/ProfileDropdown.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="profile-dropdown-wrapper">
-    <VaDropdown v-model="isShown" :offset="[9, 0]" class="profile-dropdown" stick-to-edges>
+    <VaDropdown v-model="isShown"  class="profile-dropdown" stick-to-edges>
       <template #anchor>
         <VaButton preset="secondary" color="textPrimary">
           <span class="profile-dropdown__anchor min-w-max">
             <slot />
-            <VaAvatar :size="32" color="warning"> 😍 </VaAvatar>
+            <VaAvatar :size="32"> = </VaAvatar>
           </span>
         </VaButton>
       </template>
