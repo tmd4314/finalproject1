cb6778f19cfc12fded19ac69fbacf8b35eed1d56
geniusIN <rlaghddls9309@gmail.com>
2025-06-05 16:37:31 +0900

    김홍인/주문목록/2025-06-05

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 require('dotenv').config({path : './database/configs/dbConfig.env'});
 
 const express = require('express');
-const app =express();
+const app = express();
  // 미들웨어 등록 영역
 // 1. body parser
  // content-type : application/x-www-form-urlencoded
@@ -17,11 +17,10 @@ app.listen(3000, ()=>{
 
 
  // 라우팅 등록 영역   
-// const bookRouter =require('./routers/book_router.js');
+const orderRouter = require('./routers/orderRouter');
  // 기본 라우팅
 app.get('/', (req, res)=>{
   res.send('Welcome!!...');
  })
  // 라우터 모듈 등록
-// app.use('/', bookRouter);
-
+app.use('/', orderRouter);
