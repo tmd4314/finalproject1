466f03586e911fdb52b845521aa3a332052282c2
Park Hyeon gi <hg020@naver.com>
2025-06-18 17:42:48 +0900

    박현기/수정/0618

--- server/database/sqls/prod_result.js
+++ server/database/sqls/prod_result.js
@@ -12,53 +12,50 @@ const insertResult =
 
 const selectResultList = 
 `
-  SELECT DISTINCT
-        wr.result_id,
-        wr.final_qty,
-        wrd.result_detail,
-        wrd.product_qual_qty,
-        p.product_name,
-        p.product_stand,
-        pc.process_code,
-        pc.process_name,
-        pc.process_time,
-        eq.eq_id,
-        eq.eq_name,
-        eq.last_inspection_time,
-        eq.last_cleaning_time,
-        wr.work_start_date,
-        wrd.work_start_time,
-        wrd.work_end_time,
-        wd.work_order_qty,
-        wrd.pass_qty,
-        cm.code_label AS result_code_label,
-        cd.code_label AS detail_code_label,
-        cq.code_label,
-        wrd.manager_id
-  FROM   work_result wr
-  JOIN   work_result_detail wrd
-    ON   wr.result_id = wrd.result_id
-  JOIN   work_order_master wm
-    ON   wr.work_order_no = wm.work_order_no
-  JOIN   work_order_detail wd
-    ON   wm.work_order_no = wd.work_order_no
-  JOIN   process_group pg
-    ON   wr.process_group_code = pg.process_group_code
-  JOIN   process pc
-    ON   pc.process_code = wrd.process_code 
-  JOIN   product p
-    ON   pg.product_code = p.product_code
-  JOIN   common_code cm
-    ON   wr.code_value = cm.code_value
-  JOIN   common_code cd
-    ON   wrd.code_value = cd.code_value
-  JOIN   equipment eq
-    ON   wrd.eq_id = eq.eq_id
-  JOIN   common_code cq
-    ON   eq.eq_run_code = cq.code_value
-  WHERE  wm.work_order_no = ?
-    AND  p.product_stand = ?
-  ORDER BY pc.process_code ASC;
+SELECT DISTINCT
+          wr.result_id,
+          wrd.result_detail,
+          wrd.product_qual_qty,
+          p.product_name,
+          p.product_stand,
+          pc.process_code,
+          pc.process_name,
+          pc.process_time,
+          eq.eq_id,
+          eq.eq_name,
+          wr.work_start_date,
+          wrd.work_start_time,
+          wrd.work_end_time,
+          wd.work_order_qty,
+          wrd.pass_qty,
+          cm.code_label AS result_code_label,
+          cd.code_label AS detail_code_label,
+          cq.code_label,
+          wrd.manager_id
+    FROM   work_result wr
+    JOIN   work_result_detail wrd
+      ON   wr.result_id = wrd.result_id
+    JOIN   work_order_master wm
+      ON   wr.work_order_no = wm.work_order_no
+    JOIN   work_order_detail wd
+      ON   wm.work_order_no = wd.work_order_no
+    JOIN   process_group pg
+      ON   wr.process_group_code = pg.process_group_code
+    JOIN   process pc
+      ON   pc.process_code = wrd.process_code 
+    JOIN   product p
+      ON   pg.product_code = p.product_code
+    JOIN   common_code cm
+      ON   wr.code_value = cm.code_value
+    JOIN   common_code cd
+      ON   wrd.code_value = cd.code_value
+    JOIN   equipment eq
+      ON   wrd.eq_type_code = eq.eq_type_code
+    JOIN   common_code cq
+      ON   eq.eq_run_code = cq.code_value
+    WHERE  wm.work_order_no = ?
+      AND  p.product_stand = ?
+    ORDER BY pc.process_code ASC;
 `
 ;
 
