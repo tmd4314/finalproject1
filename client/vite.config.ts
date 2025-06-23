e38304b7887fd313e1b3def030c8e1fc1239b64e
tmd4314 <110645190+tmd4314@users.noreply.github.com>
2025-06-23 12:34:56 +0900

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -70,7 +70,7 @@ const createProxy = (paths: string[]) =>
 
 export default defineConfig({
   build: {
-    sourcemap: true,
+   // sourcemap: true,
     //outDir: "../server/public",
   },
   server: {
